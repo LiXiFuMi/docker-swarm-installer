69e0b4d6ac5cf98ca93eb30774ffbf3272058cf9
Julen Lizaso <lixifumi@gmail.com>
2016-08-23 11:17:17 +0200

    First commit

---
+++ install-swarm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# author: Julen Lizaso
+# email: lixifumi@gmail.com
+
+VERSION="0.0.1"
+
+function header {
+  echo "Docker Swarm installer"
+  echo "Version ${VERSION}"
+  echo "by Julen Lizaso <lixifumi@gmail.com>"
+  echo ""
+}
+
+header
